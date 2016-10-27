4f7a12b0cc54709f636eaed0ed56d83a7d42a930
Jonathan Gordon <jgordon@jonathangordon.org>
2016-10-26 18:11:21 -0700

    Stub in interpretation.

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@
 
 import subprocess as sub
 
+from time import time
 from flask import Flask, request, jsonify
 
 from process import process_boxer
@@ -12,35 +13,64 @@ from process import process_boxer
 # - samepred: Arguments of a predicate cannot be merged.
 # - sameid: arguments of predicates with the same ID cannot be merged.
 # - freqpred: Arguments of frequent predicates cannot be merged.
-nonmerge = ['samepred', 'sameid', 'freqpred']
+nonmerge = set(['sameid', 'freqpred'])
+
+
+commands = {'tokenize':
+              ['/interpret/ext/candc/bin/t',
+               '--stdin'],
+            'candc':
+              ['/interpret/ext/candc/bin/soap_client',
+               '--url', 'localhost:8888'],
+            'boxer':
+              ['/interpret/ext/candc/bin/boxer',
+               '--stdin',
+               '--semantics', 'tacitus',
+               '--resolve', 'true'],
+            'henry':
+              []}
+
+
+def run_commands(cmds, data):
+    for cmd in cmds:
+        try:
+            p = sub.run(commands[cmd], input=data, stdout=sub.PIPE,
+                        stderr=sub.PIPE)
+            data = p.stdout
+        except Exception as e:
+            return None, 'Exception communicating with ' + cmd + '\n' + str(e)
+    return data.decode(), None
 
 
 app = Flask(__name__)
 
 @app.route('/parse', methods=['POST'])
 def parse():
-    commands = [['/interpret/ext/candc/bin/t',
-                 '--stdin'],
-                ['/interpret/ext/candc/bin/soap_client',
-                 '--url', 'localhost:8888'],
-                ['/interpret/ext/candc/bin/boxer',
-                 '--stdin',
-                 '--semantics', 'tacitus',
-                 '--resolve', 'true']]
+    data = request.get_json(force=True)['s'].encode() + b'\n'
+    out, err = run_commands(['tokenize', 'candc', 'boxer'], data)
+    if err:
+        return jsonify({'error': err})
+    return jsonify({'parse': process_boxer(out, nonmerge)})
 
+
+@app.route('/interpret', methods=['POST'])
+def interpret():
     data = request.get_json(force=True)['s'].encode() + b'\n'
 
-    for cmd in commands:
-        try:
-            p = sub.run(cmd, input=data, stdout=sub.PIPE,
-                        stderr=sub.PIPE)
-            data = p.stdout
-        except Exception as e:
-            return jsonify({'error': 'Exception communicating with ' + cmd +
-                            '\n' + str(e)})
-            break
+    out, err = run_commands(['tokenize', 'candc', 'boxer'], data)
+    if err:
+        return jsonify({'error': err})
 
-    return jsonify({'parse': process_boxer(data.decode(), nonmerge)})
+    parse = process_boxer(out, nonmerge)
+
+    data = parse.encode() + b'\n'
+    out, err = run_commands(['henry'], data)
+    if err:
+        return jsonify({'parse': parse,
+                        'error': err})
+
+    return jsonify({'parse': parse,
+                    'interpret': out})
 
 
 if __name__ == '__main__':
